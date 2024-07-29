5e39ba6aedf991b34c91b90b1fb6e001faf19537
weslleyantony <121349835+weslleyantony@users.noreply.github.com>
2024-07-29 07:04:06 -0300

    commit

--- Python Mundo 2/aula 12/aula12.py
+++ Python Mundo 2/aula 12/aula12.py
@@ -1,4 +1,4 @@
 nome = str(input("Qual é o seu nome? "))
 if nome == "Weslley":
     print("Que nome bonito")
-print("Tenha um bom dia {}!".format(nome))
+print("Tenha um bom dia, {}!".format(nome))
