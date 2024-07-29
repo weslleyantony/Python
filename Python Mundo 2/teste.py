aee75d3f131d10d128c302b411a41bfc305108bf
weslleyantony <121349835+weslleyantony@users.noreply.github.com>
2024-07-29 06:55:38 -0300

    commit

--- Python Mundo 2/teste.py
+++ Python Mundo 2/teste.py
@@ -2,5 +2,7 @@ n = float(input("Digite um número aí, seu porra: "))
 
 if n == 0:
     print("Parabéns, fez o mínimo")
+if n ==7:
+    print("numero da sorte, corno")
 else:
     print("Alo porra, tá errado caralho")
