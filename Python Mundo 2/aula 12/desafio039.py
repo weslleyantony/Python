fab260426467ba1fc4338856cf5e60cc8a704861
weslleyantony <121349835+weslleyantony@users.noreply.github.com>
2024-08-09 17:02:26 -0300

    commit
    Estudos 09/08/24

--- Python Mundo 2/aula 12/desafio039.py
+++ Python Mundo 2/aula 12/desafio039.py
@@ -1,15 +1,28 @@
 from datetime import date
 
-ano_atual = date.today().year
-ano_nascimento = int(input("Digite o ano do seu nascimento: "))
-parametro = ano_atual - ano_nascimento
-atraso = (ano_atual - ano_nascimento) - 18
-diferenca_menor = ano_nascimento - (ano_atual - 18)
-diferenca_maior = ano_atual - atraso
+atual = date.today().year
+nascimento = int(input("Digite o ano do seu nascimento: "))
+idade = atual - nascimento
+sexo = str(input('Qual o seu sexo?\nHomem (H)\nMulher (M) \n'))
 
-if parametro <= 17:
-    print("Você ainda irá se alistar daqui {} ano(s)!".format(diferenca_menor))
-elif parametro == 18:
-    print("Está na hora! Você deve se alistar este ano!")
-else:
-    print("O tempo já passou! Deveria ter se alistado há {} ano(s) atrás, em {}!".format(atraso, diferenca_maior)) 
+print('-='*20)
+
+if sexo == 'M':
+    print('Pessoas do sexo feminino não precisam realizar o alistamento!')
+elif sexo == 'H':
+    if idade == 18:
+        print('Quem nasceu no ano de {} tem {} ano(s) em {}!'.format(nascimento, idade, atual))
+        print('Você deve se alistar IMEDIATAMENTE')
+    elif idade < 18:
+        saldo = 18 - idade
+        print('Quem nasceu no ano de {} tem {} ano(s) em {}!'.format(nascimento, idade, atual))
+        print("Ainda faltam {} ano(s)!".format(saldo))
+        print('Seu alistamento será em {}.'.format(atual + saldo))
+    elif idade > 18:
+        saldo = idade - 18
+        print('Quem nasceu no ano de {} tem {} ano(s) em {}!'.format(nascimento, idade, atual))
+        print('Você está atrasado {} ano(s)!'.format(saldo)) 
+        print('Seu alistamento foi em {}.'.format(atual - saldo))
+    elif idade < 0:
+        print('Insira um ano de nascimento válido.')
+        print('Ainda estamos no ano de {}!'.format(atual))
