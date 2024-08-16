8785e1a99cfcb3137d59fb0fb7ea64e48ebec976
weslleyantony <121349835+weslleyantony@users.noreply.github.com>
2024-08-16 17:28:18 -0300

    commit
    16/08

--- Python Mundo 2/aula 12/desafio045.py
+++ Python Mundo 2/aula 12/desafio045.py
@@ -3,7 +3,11 @@ print('-='*20)
 print('JOKENPÔ com PYTHON')
 print('-='*20)
 
-escolha = str(input('Escolha entra Pedra, Papel ou Tesoura: ')).upper()
+escolha = str(input('''Escolha uma opção:
+[ 1 ] Pedra
+[ 2 ] Papel
+[ 3 ] Tesoura
+''')).upper()
 sorteio = randint(1,3)
 
 
