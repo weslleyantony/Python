3abaa09ed549310872da8e62d5c5803d4f354b7c
weslleyantony <121349835+weslleyantony@users.noreply.github.com>
2024-09-18 07:26:24 -0300

    commit
    Estudos 18/09/2024

---
+++ Python Mundo 2/aula 13/desafio054.py
@@ -0,0 +1,13 @@
+from datetime import date
+ano = date.today().year
+totmai = 0
+totmen = 0
+
+for c in range (1,8):
+    idade = int(input('Em que ano a {}° pessoa nasceu? '.format(c)))
+    dif = ano - idade
+    if dif >= 18:
+        totmai += 1
+    elif dif < 18:
+        totmen += 1
+print('Temos {} pessoa(s) maior(es) de idade e {} pessoa(s) menor(es) de idade'.format(totmai,totmen))
