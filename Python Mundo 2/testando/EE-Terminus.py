0ab2e198c6e8a017b29e7653b65dc56b315a30d5
weslleyantony <121349835+weslleyantony@users.noreply.github.com>
2024-11-13 16:18:56 -0300

    projetinho pra jogar COD
    projeto para fazer fórmula do Esater Egg do Cod BO6 para otimizar o tempo em game

---
+++ Python Mundo 2/testando/EE-Terminus.py
@@ -0,0 +1,21 @@
+print("-="*20)
+print("FÓRMULAS EASTER EGG TERMINUS COD 20BO6")
+print("-="*20)
+
+valorX = int(input("Qual o valor de X? "))
+valorY = int(input("Qual o valor de Y? "))
+valorZ = int(input("Qual o valor de Z? "))
+
+res1 = 2 * valorX + 11 
+res2 = (2 * valorZ + valorY) - 5
+res3 = (valorY + valorZ) - valorX
+
+print("-="*20)
+print("RESULTADOS")
+print("-="*20)
+
+print("Resultado 1 = {}".format(res1))
+print("Resultado 2 = {}".format(res2))
+print("Resultado 3 = {}".format(res3))
+
+print("-="*20)
